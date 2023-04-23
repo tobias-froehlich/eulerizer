8aa64c6e30fc1d347eb3dab7e7f0d3ea3297410b
tobias-froehlich <tobias.froehlich@rwth-aachen.de>
2023-04-23 15:03:13 +0200

    Create lilypond code that shows the configuration graphically.

--- Gui_standard7.py
+++ Gui_standard7.py
@@ -11,6 +11,7 @@ class Gui(tk.Frame):
         self.__make_canvas()
         self.__make_regions()
         self.__make_dots()
+        self.__writeLilypondGraphic()
 
     def __euler_to_coords(self, i, j, k):
         x = int(
@@ -48,46 +49,24 @@ class Gui(tk.Frame):
         self.__regions = []
         for i in range(self.__const["EULER_PEDALS"]):
             (x0, y0) = self.__euler_to_coords(
-                i-0.5,
+                i-0.3,
                 -0.5,
                 0
             )
-            (x1, y1) = self.__euler_to_coords(
-                i-0.5,
-                self.__const["EULER_ROWS"] - 0.5,
-                0
-            )
 
-
-            (x2, y2) = self.__euler_to_coords(
-                i-0.5,
-                -0.5,
-                1
-            )
             (x3, y3) = self.__euler_to_coords(
-                i-0.5,
-                self.__const["EULER_ROWS"] - 0.5,
-                1
+                i-0.3,
+                self.__const["EULER_ROWS"] - 0.25,
+                0
             )
             (x4, y4) = self.__euler_to_coords(
-                i+1.5,
-                self.__const["EULER_ROWS"] - 0.5,
-                1
-            )
-            (x5, y5) = self.__euler_to_coords(
-                i+1.5,
-                -0.5,
-                1
-            )
-
-
-            (x6, y6) = self.__euler_to_coords(
-                i+1.5,
-                self.__const["EULER_ROWS"] - 0.5,
+                i+1.7,
+                self.__const["EULER_ROWS"] - 0.25,
                 0
             )
+
             (x7, y7) = self.__euler_to_coords(
-                i+1.5,
+                i+1.7,
                 -0.5,
                 0
             )
@@ -104,6 +83,19 @@ class Gui(tk.Frame):
             )
         self.set_region(4)
 
+    def __getNoteName(self, i, j, k):
+        index_init = self.__const["NOTE_NAMES"].index(
+            self.__const["INIT_NAME"]
+        )
+        i_init = self.__const["INIT_POS"][0]
+        j_init = self.__const["INIT_POS"][1]
+        k_init = self.__const["INIT_POS"][2]
+        index = index_init - i_init + i - (j_init - j)*4 + (k_init - k)*2
+        if (0 <= index < len(self.__const["NOTE_NAMES"])):
+            return self.__const["NOTE_NAMES"][index]
+        else:
+            return "o"
+
     def __make_dots(self):
         self.__dots_small = []
         self.__dots_big = []
@@ -116,12 +108,6 @@ class Gui(tk.Frame):
                 self.__playing[-1].append([])
                 for i in range(self.__const["EULER_PEDALS"] + 3):
                     self.__playing[-1][-1].append(0)
-        index_init = self.__const["NOTE_NAMES"].index(
-            self.__const["INIT_NAME"]
-        )
-        i_init = self.__const["INIT_POS"][0]
-        j_init = self.__const["INIT_POS"][1]
-        k_init = self.__const["INIT_POS"][2]
         for k in range(self.__const["EULER_LAYERS"]):
             self.__dots_small.append([])
             self.__dots_big.append([])
@@ -129,26 +115,15 @@ class Gui(tk.Frame):
                 self.__dots_small[-1].append([])
                 self.__dots_big[-1].append([])
                 for i in range(self.__const["EULER_PEDALS"] + 1):
-                    index = index_init - i_init + i - (j_init - j)*4 + (k_init - k)*2
-                    if (0 <= index < len(self.__const["NOTE_NAMES"])):
-                        name = self.__const["NOTE_NAMES"][index]
-                        if k == 0:
-                            image = tk.PhotoImage(
-                                file="figs/%s_small.png"%(name)
-                            )
-                        else:
-                            image = tk.PhotoImage(
-                                file="figs/%s_small_gray.png"%(name)
-                            )
+                    name = self.__getNoteName(i, j, k)
+                    if k == 0:
+                        image = tk.PhotoImage(
+                            file="figs/%s_small.png"%(name)
+                        )
                     else:
-                        if k == 0:
-                            image = tk.PhotoImage(
-                                file="figs/o_small.png"
-                            )
-                        else:
-                            image = tk.PhotoImage(
-                                file="figs/o_small_gray.png"
-                            )
+                        image = tk.PhotoImage(
+                            file="figs/%s_small_gray.png"%(name)
+                        )
                     self.__images.append(image)
                     (x, y) = self.__euler_to_coords(
                         i, j, k
@@ -161,25 +136,14 @@ class Gui(tk.Frame):
                             anchor=tk.CENTER
                         )
                     )
-                    if (0 <= index < len(self.__const["NOTE_NAMES"])):
-                        name = self.__const["NOTE_NAMES"][index]
-                        if k == 0:
-                            image = tk.PhotoImage(
-                                file="figs/%s_big.png"%(name)
-                            )
-                        else:
-                            image = tk.PhotoImage(
-                                file="figs/%s_big_gray.png"%(name)
-                            )
+                    if k == 0:
+                        image = tk.PhotoImage(
+                            file="figs/%s_big.png"%(name)
+                        )
                     else:
-                        if k == 0:
-                            image = tk.PhotoImage(
-                                file="figs/o_big.png"
-                            )
-                        else:
-                            image = tk.PhotoImage(
-                                file="figs/o_big_gray.png"
-                            )
+                        image = tk.PhotoImage(
+                            file="figs/%s_big_gray.png"%(name)
+                        )
                     self.__images.append(image)
                     (x, y) = self.__euler_to_coords(
                         i, j, k
@@ -246,4 +210,75 @@ class Gui(tk.Frame):
                         self.__dots_big[k][j][i],
                         state=tk.HIDDEN
                     )
-   
+  
+    def __writeLilypondGraphic(self):
+        pedalsToPlot = range(8)
+        columnsToPlot = []
+        for p in pedalsToPlot:
+            for c in [p, p + 1]:
+                if c not in columnsToPlot:
+                     columnsToPlot.append(c)
+        shift = min(columnsToPlot)
+        with open("/tmp/eulerizerplot.txt", "w") as f:
+            f.write("\\version \"2.22.1\"\n")
+            f.write("\\book {\n")
+            f.write("\\markup {\n")
+            scaling = 0.07
+            pedalNames = [
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \\char ##x2212 \\char ##x2163 }",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \\char ##x2212 \\char ##x2162 }",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \\char ##x2212 \\char ##x2161 }",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \\char ##x2212 \\char ##x2160 }",
+                "\\fontsize #3 \\italic \\sans \"O\"",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \"+\" \\char ##x2160 }",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \"+\" \\char ##x2161 }",
+                "\\fontsize #3 \\italic \\concat { \\raise #0.35 \"+\" \\char ##x2162 }",
+            ]
+            for k in range(self.__const["EULER_LAYERS"]):
+                for j in range(self.__const["EULER_ROWS"]):
+                    for i in columnsToPlot:
+                         (x, y) = self.__euler_to_coords(i - shift, j, k)
+                         (x, y) = (x*scaling, y*scaling)
+                         notename = self.__getNoteName(i, j, k)
+                         f.write("\\combine ")
+                         if True:
+                             if len(notename) == 1:
+                                 notename = "\\fontsize #3 " + notename[0]
+                             elif len(notename) == 2:
+                                 if notename[1] == "#":
+                                     notename = "\\fontsize #3 " + notename[0] + " \\fontsize #0.3 \\raise #1.2 \\sharp"
+                                 elif notename[1] == "b":
+                                     notename = "\\fontsize #3 " + notename[0] + " \\fontsize #0.5 \\raise #0.3 \\flat"
+                         elif k == 1:
+                             if len(notename) == 1:
+                                 notename = "\\fontsize #1 " + notename[0]
+                             elif len(notename) == 2:
+                                 if notename[1] == "#":
+                                     notename = "\\fontsize #0.5 " + notename[0] + " \\fontsize #0.05 \\raise #1.2 \\sharp"
+                                 elif notename[1] == "b":
+                                     notename = "\\fontsize #0.5 " + notename[0] + " \\fontsize #0.07 \\raise #0.3 \\flat"
+                         f.write("\\translate #'(%f . %f) \\concat \\center-align { %s }"%(x, -y, notename))
+            f.write("\n")                       
+            for i in pedalsToPlot: 
+                n = i - shift
+                m = max(pedalsToPlot)
+                (x0, y0) = self.__euler_to_coords(-0.3 + n, -0.3 - n * 0.1, 0)
+                (x1, y1) = self.__euler_to_coords(-0.3 + n, self.__const["EULER_ROWS"] - 0.2 + (m - i) * 0.1, 0)
+                (x2, y2) = self.__euler_to_coords(-0.3 + n + 1.9, self.__const["EULER_ROWS"] - 0.2 + (m - i) * 0.1, 0)
+                (x3, y3) = self.__euler_to_coords(-0.3 + n + 1.9, -0.3 - n * 0.1, 0)
+                (x0, y0) = (x0 * scaling, y0 * scaling)
+                (x1, y1) = (x1 * scaling, y1 * scaling)
+                (x2, y2) = (x2 * scaling, y2 * scaling)
+                (x3, y3) = (x3 * scaling, y3 * scaling)
+                f.write("\\combine ")
+                f.write("\path #0.2 #'((moveto %f %f) (lineto %f %f) (lineto %f %f) (lineto %f %f) (lineto %f %f))\n"
+                %(x0, -y0, x1, -y1, x2, -y2, x3, -y3, x0, -y0))
+                (x, y) = self.__euler_to_coords(n, -0.8 - n * 0.1, 0)
+                (x, y) = (x * scaling, y * scaling)
+                pedalName = pedalNames[i]
+                f.write("\\combine")
+                f.write("\\translate #'(%f . %f) \\concat \\center-align { %s }"%(x, -y, pedalName))
+            f.write(" \" \"\n")
+            f.write("}\n")
+            f.write("}\n")                         
+                                  
